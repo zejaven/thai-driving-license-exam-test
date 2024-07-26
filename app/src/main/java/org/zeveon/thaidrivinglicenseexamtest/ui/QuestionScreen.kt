6cd190762492298418fb133fc5ad8fb97620e1b0
Vafin Stanislav <stasvafin@gmail.com>
2024-07-26 23:44:56 +0700

    making boxes from options

--- app/src/main/java/org/zeveon/thaidrivinglicenseexamtest/ui/QuestionScreen.kt
+++ app/src/main/java/org/zeveon/thaidrivinglicenseexamtest/ui/QuestionScreen.kt
@@ -1,9 +1,14 @@
 package org.zeveon.thaidrivinglicenseexamtest.ui
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
@@ -14,11 +19,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import org.zeveon.thaidrivinglicenseexamtest.model.QuestionViewModel
 
+@SuppressLint("UnusedBoxWithConstraintsScope")
 @Composable
 fun QuestionScreen(navController: NavController, viewModel: QuestionViewModel, category: String?) {
     val question by viewModel.currentQuestion.collectAsState()
@@ -38,20 +46,34 @@ fun QuestionScreen(navController: NavController, viewModel: QuestionViewModel, c
             Spacer(modifier = Modifier.height(16.dp))
 
             question?.let {
-                Text(text = it.question, style = MaterialTheme.typography.headlineLarge)
-                Spacer(modifier = Modifier.height(8.dp))
                 Text(
-                    text = it.optionA,
-                    modifier = Modifier.clickable { checkAnswer(it.optionA, it.answer) })
-                Text(
-                    text = it.optionB,
-                    modifier = Modifier.clickable { checkAnswer(it.optionB, it.answer) })
-                Text(
-                    text = it.optionC,
-                    modifier = Modifier.clickable { checkAnswer(it.optionC, it.answer) })
-                Text(
-                    text = it.optionD,
-                    modifier = Modifier.clickable { checkAnswer(it.optionD, it.answer) })
+                    text = it.question,
+                    style = MaterialTheme.typography.headlineLarge,
+                    color = MaterialTheme.colorScheme.onBackground,
+                    modifier = Modifier.padding(bottom = 16.dp)
+                )
+
+                val options = listOf(it.optionA, it.optionB, it.optionC, it.optionD)
+
+                Column(modifier = Modifier.fillMaxWidth()) {
+                    options.forEach { option ->
+                        Box(
+                            modifier = Modifier
+                                .padding(8.dp)
+                                .border(1.dp, Color.Black)
+                                .clickable { checkAnswer(option, it.answer) }
+                                .background(MaterialTheme.colorScheme.surface)
+                                .padding(16.dp)
+                                .fillMaxWidth(),
+                            contentAlignment = Alignment.CenterStart
+                        ) {
+                            Text(
+                                text = option,
+                                color = MaterialTheme.colorScheme.onBackground
+                            )
+                        }
+                    }
+                }
             }
         }
     }
